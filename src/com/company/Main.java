d467ce9ab3077f8793e9ed095a64837911c0f772
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 21:09:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter you're color code");
+        int V = in.nextInt();
+        switch (V) {
+            case 3:
+                System.out.println("Next traffic light is green" + "Enter a color code");}
+        System.out.println("Try again please type 5.");
+        int l = in.nextInt();
+        switch (l) {
+            case 5:
+                System.out.println("Enter you're color code");}
+        System.out.println("\"Next traffic light is red\" + \"Enter your color code\"");
+        int z = in.nextInt();
+        switch (z) {
+            case 78:
+                System.out.println("Next traffic light is yellow");}
+        
+
+
+    }
+}
